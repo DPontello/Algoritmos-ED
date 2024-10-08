dc2e216b53093c2d8d30774e1d0387ba0af1ae0d
cubo <hpontello81@gmail.com>
2024-10-08 17:49:39 -0300

    desgracadepilha

--- Estrutura-de-dados/Exemplos Aula/Pilha1.cpp
+++ Estrutura-de-dados/Exemplos Aula/Pilha1.cpp
@@ -45,3 +45,109 @@ Nome: um Tipo: a Valor: 1
 
 #include <iostream>
 using namespace std;
+
+struct Item {
+    string nome;
+    char tipo;
+    int valor;
+};
+
+class Noh{
+    friend class Pilha;
+    private: 
+    Item item;
+    Noh *mAnterior;
+
+    public:
+    Noh(Item item){
+        this->item = item;
+        mAnterior = NULL;
+    }
+
+};
+
+class Pilha{
+    private:
+    Noh *mTopo;
+    int mTamanho;
+    public:
+    Pilha(){
+        mTopo = NULL;
+        mTamanho = 0;
+    }
+    ~Pilha(){
+        while(!vazia()){
+            desempilha();
+        }
+    }
+    bool vazia(){
+        return (mTopo == NULL);
+    }
+    void empilha(Item item){
+        Noh *novoNoh = new Noh(item);
+        novoNoh->mAnterior = mTopo;
+        mTopo = novoNoh;
+        mTamanho++;
+    }
+    void desempilha(){
+        if(vazia()){
+            cout << "Erro: pilha vazia!" << endl;
+            return;
+        }
+        Noh *aux = mTopo;
+        mTopo = mTopo->mAnterior;
+        delete aux;
+        mTamanho--;
+    }
+    void espiarTopo(){
+        if(vazia()){
+            cout << "Erro: pilha vazia!" << endl;
+            return;
+        }
+        cout << "Nome: " << mTopo->item.nome << " Tipo: " << mTopo->item.tipo << " Valor: " << mTopo->item.valor << endl;
+    }
+
+    void limparPilha(){
+        while(!vazia()){
+            desempilha();
+            espiarTopo();
+        }
+    }
+};
+
+int main(){
+    
+    Pilha pilha;
+    Item item;
+    char comando;
+    bool sexo = false;
+    while (sexo == false) {
+        cin >> comando;
+
+        if (comando == 'i') {
+            // Comando para inserir um item
+            cin >> item.nome >> item.tipo >> item.valor;
+            pilha.empilha(item);
+        }
+        else if (comando == 'r') {
+            // Comando para remover/desempilhar o item do topo
+            pilha.espiarTopo();
+            pilha.desempilha();
+        }
+        else if (comando == 'e') {
+            // Comando para espiar o topo da pilha sem remover
+            pilha.espiarTopo();
+        }
+        else if (comando == 'l') {
+            // Comando para limpar a pilha (remover todos os elementos)
+            pilha.limparPilha();
+        }
+        else if (comando == 'f') {
+            // Comando para finalizar o programa, imprimindo todos os elementos restantes
+            pilha.limparPilha(); 
+            sexo = true;
+            }
+        }
+
+    return 0;
+}
