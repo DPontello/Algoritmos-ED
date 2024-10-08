c884ee04c1bc57e611b73c2ce282927953781f6b
cubo <hpontello81@gmail.com>
2024-10-08 17:14:51 -0300

    exercicioPilha1

--- Estrutura-de-dados/Exemplos Aula/exercicioPilha1.cpp
+++ Estrutura-de-dados/Exemplos Aula/exercicioPilha1.cpp
@@ -6,3 +6,97 @@ B e C, verifique se w faz parte ou não do conjunto de sentenças de L. Utilize 
 
 #include <iostream>
 using namespace std;
+
+class Noh{
+    private: 
+    char mValor;
+    Noh *mAnterior;
+
+    public:
+    Noh(char valor);
+    char getValor(){return mValor;}
+    void setAnterior(Noh *anterior);
+    Noh *getAnterior(){return mAnterior;}
+};
+
+Noh::Noh(char mValor){
+    this->mValor = mValor;
+    mAnterior = NULL;
+}
+
+void Noh::setAnterior(Noh *mAnterior){
+    this->mAnterior = mAnterior;
+}
+
+class Pilha{
+    private:
+    Noh *mTopo;
+    unsigned tamanho;
+
+    public:
+    Pilha();
+    ~Pilha();
+    unsigned getTamanho(){return tamanho;}
+    void empilha(char valor);
+    bool estaVazia(){return (tamanho ==0);}
+    char desempilha();
+    void limparPilha();
+};
+
+Pilha::Pilha(){
+    mTopo = NULL;
+    tamanho = 0;
+}
+
+Pilha::~Pilha(){
+    limparPilha();
+}
+
+void Pilha::empilha(char valor){
+    Noh *novoNo = new Noh(valor);
+    novoNo->setAnterior(mTopo);
+    mTopo = novoNo;
+    tamanho++;
+}
+
+char Pilha::desempilha(){
+    char valor = mTopo->getValor();
+    Noh *aux = mTopo;
+    mTopo = aux->getAnterior();
+    delete aux;
+    tamanho--;
+    return valor;
+}
+
+void Pilha::limparPilha(){
+    while(!estaVazia()){
+        desempilha();
+    }
+}
+
+int main(){
+    Pilha lista;
+    string sequencia;
+    cin >> sequencia;
+
+    int i = 0;
+    while (sequencia[i]){
+        lista.empilha(sequencia[i]);
+        i++;
+    }
+
+    int cont[2] = {0,0};
+    while (not lista.estaVazia()){
+        char letra = lista.desempilha();
+        if (letra == 'B') cont[1]++;
+        else if (letra =='A') cont[0]++;
+    }
+
+    if (cont[0] == cont[1]){
+        cout << "Essa sequência é válida!!!" << endl;
+    } else {
+        cout << "Essa sequência não é válida!!!" << endl;
+    }
+
+    return 0;
+}
