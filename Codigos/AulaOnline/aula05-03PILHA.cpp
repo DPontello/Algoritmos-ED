f1784250560abae92e9e473c8e20c41e4cc7324b
cubo <hpontello81@gmail.com>
2024-10-09 18:29:14 -0300

    ExercicioestruturaPilha1

--- Codigos/AulaOnline/aula05-03PILHA.cpp
+++ Codigos/AulaOnline/aula05-03PILHA.cpp
@@ -14,6 +14,7 @@ class Noh{
 Noh::Noh(int valor){
     mValor = valor;
     mProximo = NULL;
+
 }
 
 class Pilha{
--- Estrutura-de-dados/Exemplos Aula/exercicioPilha1.cpp
+++ Estrutura-de-dados/Exemplos Aula/exercicioPilha1.cpp
@@ -8,95 +8,59 @@ B e C, verifique se w faz parte ou não do conjunto de sentenças de L. Utilize 
 using namespace std;
 
 class Noh{
+    friend class Pilha;
     private: 
-    char mValor;
-    Noh *mAnterior;
+        char letra;
+        Noh *mProximo; 
+    
+    public: 
+        Noh(char letra){
+            this->letra = letra;
+            mProximo = NULL;
+        }    
 
-    public:
-    Noh(char valor);
-    char getValor(){return mValor;}
-    void setAnterior(Noh *anterior);
-    Noh *getAnterior(){return mAnterior;}
 };
-
-Noh::Noh(char mValor){
-    this->mValor = mValor;
-    mAnterior = NULL;
-}
-
-void Noh::setAnterior(Noh *mAnterior){
-    this->mAnterior = mAnterior;
-}
-
+   
 class Pilha{
-    private:
-    Noh *mTopo;
-    unsigned tamanho;
-
+    private: 
+        Noh *mTopo;
+        unsigned mTamanho;
+    
     public:
-    Pilha();
-    ~Pilha();
-    unsigned getTamanho(){return tamanho;}
-    void empilha(char valor);
-    bool estaVazia(){return (tamanho ==0);}
-    char desempilha();
-    void limparPilha();
+        Pilha(){
+            mTopo = NULL;
+            mTamanho = 0;
+        }
+        
+        ~Pilha(){
+            limpaPilha();
+        }
+
+        void push(char letra){
+            Noh *novoNoh = new Noh(letra);
+            novoNoh->mProximo = mTopo; 
+            mTopo = novoNoh;
+            mTamanho++;
+        }
+
+        void limpaPilha(){
+            while(mTamanho > 0){
+                pop();
+            }
+        }
+
+        int pop(){
+            int valor = mTopo->letra;
+            Noh *aux = mTopo;
+            mTopo = mTopo->mProximo;
+            delete aux;
+            mTamanho--;
+            return valor;
+
+        }
+
+        bool isEmpty(){
+            return (mTopo == NULL);
+        }
+        
 };
-
-Pilha::Pilha(){
-    mTopo = NULL;
-    tamanho = 0;
-}
-
-Pilha::~Pilha(){
-    limparPilha();
-}
-
-void Pilha::empilha(char valor){
-    Noh *novoNo = new Noh(valor);
-    novoNo->setAnterior(mTopo);
-    mTopo = novoNo;
-    tamanho++;
-}
-
-char Pilha::desempilha(){
-    char valor = mTopo->getValor();
-    Noh *aux = mTopo;
-    mTopo = aux->getAnterior();
-    delete aux;
-    tamanho--;
-    return valor;
-}
-
-void Pilha::limparPilha(){
-    while(!estaVazia()){
-        desempilha();
-    }
-}
-
-int main(){
-    Pilha lista;
-    string sequencia;
-    cin >> sequencia;
-
-    int i = 0;
-    while (sequencia[i]){
-        lista.empilha(sequencia[i]);
-        i++;
-    }
-
-    int cont[2] = {0,0};
-    while (not lista.estaVazia()){
-        char letra = lista.desempilha();
-        if (letra == 'B') cont[1]++;
-        else if (letra =='A') cont[0]++;
-    }
-
-    if (cont[0] == cont[1]){
-        cout << "Essa sequência é válida!!!" << endl;
-    } else {
-        cout << "Essa sequência não é válida!!!" << endl;
-    }
-
-    return 0;
-}
