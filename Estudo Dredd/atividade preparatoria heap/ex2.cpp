b24bcde4c3e34c9f67e497bc6a6e0631c6cbe1ee
cubo <hpontello81@gmail.com>
2024-10-30 14:25:09 -0300

    ajuste atividade 2 do heap preparatorio

--- Estudo Dredd/atividade preparatoria heap/ex2.cpp
+++ Estudo Dredd/atividade preparatoria heap/ex2.cpp
@@ -41,75 +41,81 @@ Exemplo de Saída:
 *
 */
 #include <iostream>
-#include <cstring>
-#include <algorithm> 
+#include <algorithm>
 using namespace std;
 
 const int INVALIDO = -1;
 
-class torneio{
+class Torneio {
 private:
-	int *heap;
-	int tamanho;
-	int capacidade;
-    int inicioDados;
-	inline int pai(int filho);
-	inline int esquerda(int getPai);
-	inline int direita(int getPai);
-	void arruma();
-	void compete(int i);
-    void verBaseMaior();
+    int *heap;  
+    int tamanho; 
+    int capacidade; // Quantidade de folhas (participantes)
+    int inicioDados; // Índice onde começam as folhas
+
+    inline int pai(int filho) { return (filho - 1) / 2; }
+    inline int esquerda(int pai) { return 2 * pai + 1; }
+    inline int direita(int pai) { return 2 * pai + 2; }
+    void compete(int i);
 
 public:
-	torneio(int vet[], int tam);
-	~torneio();
+    Torneio(int vet[], int tam);
+    ~Torneio();
+    int campeao(); 
 };
 
-//Construtor da classe
-torneio::torneio(int vet[], int tam){ 
+Torneio::Torneio(int vet[], int tam) {
+    capacidade = tam;
 
+    tamanho = 2 * capacidade - 1;
+    heap = new int[tamanho];
+
+    inicioDados = capacidade - 1;
+
+    for (int i = 0; i < capacidade; i++) {
+        heap[inicioDados + i] = vet[i];
+    }
+
+    for (int i = inicioDados - 1; i >= 0; i--) {
+        compete(i);
+    }
 }
 
-// Destrutor da classe
-torneio::~torneio(){
-
+Torneio::~Torneio() {
+    delete[] heap;
 }
 
-// Retorno a posição do nó pai de um determinado elemento
-int torneio::pai(int filho){  
-    return (filha-1)/2;
+// Função para fazer a competição entre os elementos
+void Torneio::compete(int i) {
+    int esq = esquerda(i);
+    int dir = direita(i);
+
+
+    if (esq < tamanho && dir < tamanho) {
+        heap[i] = max(heap[esq], heap[dir]);
+    } else if (esq < tamanho) {
+        heap[i] = heap[esq];
+    } else if (dir < tamanho) {
+        heap[i] = heap[dir];
+    }
 }
 
-// Retorna o filho à esquerda de um determinado nó
-int torneio::esquerda(int pai){ 
-    return pai*2+1
+
+int Torneio::campeao() {
+    return heap[0];
 }
 
-// Retorna o filho à direita do nó pai
-int torneio::direita(int pai){ 
-    return pai*2+2;
-}
-
-// Faz a competição por cada posição vencedora no vetor e imprime o vencedor
-void torneio::arruma(){
-    for(int i = inicioDados; i < )
-}
-
-// Faz a competição entre os elementos
-void torneio::compete(int i){ 
-
-}
-
-int main(){
+int main() {
     int tam;
     cin >> tam;
 
     int vet[tam];
-    for(int i = 0; i < tam; i++){
+    for (int i = 0; i < tam; i++) {
         cin >> vet[i];
     }
 
-    torneio participantes(vet, tam);
+    Torneio participantes(vet, tam);
+    cout << participantes.campeao() << endl;
 
     return 0;
 }
